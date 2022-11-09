07beb6050485d5025f4c1bbe7754f1846b2f39bc
Shuai Shao <shuai@rackham3.uppmax.uu.se>
2022-11-09 16:04:58 +0100

    Added file for filtering isotypes.

---
+++ 2-common/1-scripts/0-1-filter_isotypes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import subprocess
+import csv
+isotypes = set()
+with open("../../1-data/CelegansStrainData.tsv", "r") as file:
+    strains = csv.reader(file, delimiter = "\t")
+    header = True
+    for line in strains:
+        if (header):
+            header = False
+        else:
+            # print(line)
+            isotypes.add(line[3])
+
+for isotype in isotypes:
+        subprocess.run(["cp", f"../../1-data/1-raw/strain_bams/{isotype}.bam.bai", f"../../1-data/2-isotypes/"])
