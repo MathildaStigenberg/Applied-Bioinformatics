be0892c51e30d1f4029ccb30d5a2100d9ffe4a3c
Mathilda Stigenberg <mathi@rackham3.uppmax.uu.se>
2022-11-23 15:52:41 +0100

    Remove renamed files

--- 2-common/1-scripts/1-3-reproduce-5-samples-vcf.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env/python3
-
-import os
-import subprocess
-path = "/proj/snic2022-6-224/private/Applied-Bioinformatics/1-data/5-mtdna-vcf-cendr/1-2-mtdna-vcf.vcf.gz"
-output = "/proj/snic2022-6-224/private/Applied-Bioinformatics/1-data/6-mtdna-vcf-subset-cendr/1-3-mtdna-subset.vcf.gz"
-
-subprocess.Popen(f"bcftools view -s MY23,ECA1283,GXW1,NIC1,JU2800 {path} -o {output} -O z", shell = True)
--- 2-common/1-scripts/101-reproduce-vc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-module load bioinfo-tools GATK/4.1.4.1 fasdtp/0.20.0 \
-bwa/0.7.17 sambamba/0.7.1 picard/2.23.4 bcftools/1.10 \
-snpEff/4.3t Beagle/5.1
-
-
-#MISSING: VCF2Phylips, BioConvert 0.3.0, QuickTree 2.5, FigTree 1.4.4
-
