74f70c45780b68df9a02f7e0698b974a4c0924b1
Mathilda Stigenberg <mathi@rackham3.uppmax.uu.se>
2022-12-08 16:15:14 +0100

    dictionary with sites as keys and genes as values, script 112

---
+++ 2-common/1-scripts/112-genes.r
@@ -0,0 +1,113 @@
+genes <- c()
+
+for (i in c(1:55)){
+  genes[[i]] <- "tRNA-Pro"
+}
+for (i in c(58:112)){
+  genes[[i]] <- "tRNA-Val"
+}
+for (i in c(113:547)){
+  genes[[i]] <- "ND6"
+}
+for (i in c(549:782)){
+  genes[[i]] <- "ND4L"
+}
+for (i in c(785:841)){
+  genes[[i]] <- "tRNA-Trp"
+}
+for (i in c(842:897)){
+  genes[[i]] <- "tRNA-Glu"
+}
+for (i in c(898:1594)){
+  genes[[i]] <- "s-rRNA"
+}
+for (i in c(1595:1647)){
+  genes[[i]] <- "tRNA-Ser"
+}
+for (i in c(1648:1703)){
+  genes[[i]] <- "tRNA-Asn"
+}
+for (i in c(1707:1762)){
+  genes[[i]] <- "tRNA-Tyr"
+}
+for (i in c(1763:2638)){
+  genes[[i]] <- "ND1"
+}
+for (i in c(2634:3233)){
+  genes[[i]] <- "ATP6"
+}
+for (i in c(3244:3306)){
+  genes[[i]] <- "tRNA-Lys"
+}
+for (i in c(3307:3361)){
+  genes[[i]] <- "tRNA-Leu"
+}
+for (i in c(3362:3417)){
+  genes[[i]] <- "tRNA-Ser2"
+}
+for (i in c(3418:4266)){
+  genes[[i]] <- "ND2"
+}
+for (i in c(4269:4329)){
+  genes[[i]] <- "tRNA-Ile"
+}
+for (i in c(4330:4384)){
+  genes[[i]] <- "tRNA-Arg"
+}
+for (i in c(4385:4439)){
+  genes[[i]] <- "tRNA-Gln"
+}
+for (i in c(4447:4503)){
+  genes[[i]] <- "tRNA-Phe"
+}
+for (i in c(4504:5616)){
+  genes[[i]] <- "CYTB"
+}
+for (i in c(5621:5677)){
+  genes[[i]] <- "tRNA-Leu2"
+}
+for (i in c(5678:6445)){
+  genes[[i]] <- "COX3"
+}
+for (i in c(6450:6505)){
+  genes[[i]] <- "tRNA-Thr"
+}
+for (i in c(6506:7735)){
+  genes[[i]] <- "ND4"
+}
+for (i in c(7736:7844)){
+  genes[[i]] <- "Intergenic-region"
+}
+for (i in c(7845:9422)){
+  genes[[i]] <- "COX1"
+}
+for (i in c(9422:9477)){
+  genes[[i]] <- "tRNA-Cys"
+}
+for (i in c(9478:9537)){
+  genes[[i]] <- "tRNA-Met"
+}
+for (i in c(9538:9592)){
+  genes[[i]] <- "tRNA-Asp"
+}
+for (i in c(9593:9648)){
+  genes[[i]] <- "tRNA-Gly"
+}
+for (i in c(9649:10344)){
+  genes[[i]] <- "COX2"
+}
+for (i in c(10348:10402)){
+  genes[[i]] <- "tRNA-His"
+}
+for (i in c(10403:11355)){
+  genes[[i]] <- "l-rRNA"
+}
+for (i in c(11356:11691)){
+  genes[[i]] <- "ND3"
+}
+for (i in c(11691:13274)){
+  genes[[i]] <- "ND5"
+}
+for (i in c(13275:13328)){
+  genes[[i]] <- "tRNA-Ala"
+}
