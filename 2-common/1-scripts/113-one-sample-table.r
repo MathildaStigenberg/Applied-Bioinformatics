5c9cc17b6aefb1e2fd38e117c3ea20de7b1abf60
Mathilda Stigenberg <mathi@rackham3.uppmax.uu.se>
2022-12-14 16:30:47 +0100

    Added effect and type, changed context for script 113

--- 2-common/1-scripts/113-one-sample-table.r
+++ 2-common/1-scripts/113-one-sample-table.r
@@ -6,13 +6,13 @@ library("xtable")
 
 args <- commandArgs(trailingOnly=TRUE)
 
-# The r-script which generates a dictionary with all sites as keys and genes as values
+# The r-script (112-genes.r)  which generates a dictionary with all sites as keys and genes as values
 gene_script <- args[1]
 
 # The mtdna reference of c. elegans, a fa. file 
 reference <- args[2]
 
-# A .vcf file 
+# An annotated .vcf file 
 vcf_file <- args[3]
 
 # The chosen sample one wants to investigate the variants for 
@@ -51,8 +51,12 @@ Isotype <- c()
 Position <- c()
 Gene <- c()
 Mutation <- c()
+Effect <- c()
 Frequency <- c()
 Context <- c()
+Type <- c()
+
+data_vcf_fix[c("base","effect")] <- str_split_fixed(data_vcf_fix$ANN,"[|]",3)
 
 len <- length(data_vcf$gt$gt_GT)
 for (i in 1:len){
@@ -61,23 +65,58 @@ for (i in 1:len){
   }
   if (data_vcf$gt$gt_GT[i] == 1 & data_vcf$gt$Indiv[i] == sample){
     for (j in data_vcf$gt$POS[i]){
-      if (!is.null(genes[[j]])){ 
-        seq <- gsub(", ", "", toString(fasta[(j-4):(j+4)]))
-        Isotype <- append(Isotype, data_vcf$gt$Indiv[i])
-        Position <- append(Position, j)
+      if (nchar(data_vcf$fix$REF[data_vcf$fix$POS == j]) > 1){
+        seq_left <- gsub(", ", "", toString(fasta[(j-8):j]))
+        seq_right <- gsub(", ", "", toString(fasta[(j+nchar(data_vcf$fix$REF[data_vcf$fix$POS == j])):(j+nchar(data_vcf$fix$REF[data_vcf$fix$POS == j])+6)]))
+        sequence <- sprintf("%s[...]%s", toupper(seq_left), toupper(seq_right))
+        Context <- append(Context, sequence)
+      }
+      else {
+        seq <- gsub(", ", "", toString(fasta[(j-8):(j+8)]))
+        Context <- append(Context, toupper(seq))
+      }
+      Isotype <- append(Isotype, data_vcf$gt$Indiv[i])
+      Position <- append(Position, j)
+      variant <- sprintf("%s->%s", data_vcf$fix$REF[data_vcf$fix$POS == j], data_vcf$fix$ALT[data_vcf$fix$POS == j])
+      Mutation <- append(Mutation, variant)
+      annotation_effect <- gsub("_", " ", data_vcf_fix$effect[data_vcf_fix$POS == j])
+      annotation_effect <- gsub("&", " & ", annotation_effect)
+      annotation_effect <- gsub("variant", "", annotation_effect)
+      Effect <- append(Effect, annotation_effect)
+      Frequency <- append(Frequency, freq_adjusted[i])
+      if (!is.null(genes[[j]])){
         Gene <- append(Gene, genes[[j]])
-        variant <- sprintf("%s->%s", data_vcf$fix$REF[data_vcf$fix$POS == j], data_vcf$fix$ALT[data_vcf$fix$POS == j])
-        Mutation <- append(Mutation, variant)
-        Frequency <- append(Frequency, freq_adjusted[i])
-        Context <- append(Context, seq)
+      }
+      else {
+        Gene <- append(Gene, " ")
+      }
+      if (nchar(data_vcf$fix$REF[data_vcf$fix$POS == j]) == 1 & nchar(data_vcf$fix$ALT[data_vcf$fix$POS == j]) == 1){
+        if ((data_vcf$fix$REF[data_vcf$fix$POS == j] == "A" & data_vcf$fix$ALT[data_vcf$fix$POS == j] == "G") | (data_vcf$fix$REF[data_vcf$fix$POS == j] == "C" & data_vcf$fix$ALT[data_vcf$fix$POS == j] == "T") | (data_vcf$fix$REF[data_vcf$fix$POS == j] == "G" & data_vcf$fix$ALT[data_vcf$fix$POS == j] == "A") | (data_vcf$fix$REF[data_vcf$fix$POS == j] == "T" & data_vcf$fix$ALT[data_vcf$fix$POS == j] == "C")){
+          Type <- append(Type, "transition")
+        }
+        else {
+          Type <- append(Type, "transversion")
+        }
+      }
+      else if (nchar(data_vcf$fix$REF[data_vcf$fix$POS == j]) > nchar(data_vcf$fix$ALT[data_vcf$fix$POS == j])) {
+        Type <- append(Type, "deletion")
+      }
+      else if (nchar(data_vcf$fix$REF[data_vcf$fix$POS == j]) < nchar(data_vcf$fix$ALT[data_vcf$fix$POS == j])) {
+        Type <- append(Type, "insertion")
+      }
+      else {
+        Type <- append(Type, " ")
       }
     }
   }
 }
 
-info_table <- data.frame(Isotype, Position, Gene, Mutation, Frequency, Context)
+info_table <- data.frame(Isotype, Position, Gene, Mutation, Effect, Type, Frequency, Context)
 
 html_table <- print(xtable(info_table, digits = 4), type = "HTML", include.rownames = FALSE, quote=FALSE)
 
 file.create(output_table)
 write.table(html_table, output_table, row.names = FALSE, col.names = FALSE, quote=FALSE)
+
+
+
