46aba284a8e86c25e7d5ca5681b038a298163f25
Mathilda Stigenberg <mathi@rackham2.uppmax.uu.se>
2022-12-21 10:52:52 +0100

    index the mtdna bam files, script 003

--- 2-common/1-scripts/003-index-mtdna.py
+++ 2-common/1-scripts/003-index-mtdna.py
@@ -7,5 +7,9 @@ path = "/proj/snic2022-6-224/private/Applied-Bioinformatics/1-data/003-mtdna"
 
 files = os.listdir(path)
 
-for f in files:
+filter = [".bai"]
+
+bamfiles = [s for s in files if all(a not in s for a in filter)]
+
+for f in bamfiles:
     subprocess.Popen(f"samtools index {path}/{f}", shell = True)
