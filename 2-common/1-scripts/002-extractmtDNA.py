f8a84605eb3b44fca9edfca047d85208991d299a
Mathilda Stigenberg <mathi@rackham2.uppmax.uu.se>
2022-12-21 10:51:46 +0100

    extract mtdna from bam-files, script 002

--- 2-common/1-scripts/002-extractmtDNA.py
+++ 2-common/1-scripts/002-extractmtDNA.py
@@ -2,14 +2,18 @@
 
 import os
 import subprocess
+from glob import glob
 path = "/proj/snic2022-6-224/private/Applied-Bioinformatics/1-data/002-isotypes"
 files = os.listdir(path)
 
-for file in files:
-    bamfile = path + "/" + file
-    subprocess.Popen(f"samtools view {bamfile} MtDNA -b -o /proj/snic2022-6-224/private/Applied-Bioinformatics/1-data/003-mtdna/{file}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
+filter = [".bai"]
 
+bamfiles = [s for s in files if all(a not in s for a in filter)]
+
+for file in bamfiles:
+    bamfile = path + "/" + file
+    subprocess.Popen(f" samtools view {bamfile} MtDNA -b -o /proj/snic2022-6-224/private/Applied-Bioinformatics/1-data/003-mtdna/{file}", shell = True)
+     
 
 
 
