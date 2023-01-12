644fd52052bea554b3196be6ab5710688909f51b
Mathilda Stigenberg <mathi@rackham2.uppmax.uu.se>
2023-01-12 09:49:24 +0100

    Subset all 544 strains for script 103

--- 2-common/1-scripts/103-cendr-mtdna-subset-vcf.py
+++ 2-common/1-scripts/103-cendr-mtdna-subset-vcf.py
@@ -5,6 +5,6 @@ import subprocess
 
 path = "/proj/snic2022-6-224/private/Applied-Bioinformatics/1-data/102-mtdna-vcf-cendr/1-2-mtdna-vcf.vcf.gz"
 
-output = "/proj/snic2022-6-224/private/Applied-Bioinformatics/1-data/103-mtdna-subset-cendr/mtdna-vcf-subset.vcf.gz"
+output = "/proj/snic2022-6-224/private/Applied-Bioinformatics/1-data/103-mtdna-subset-cendr/mtdna-vcf-544.vcf.gz"
 
-subprocess.Popen(f"bcftools view -s MY23,ECA1283,GXW1,NIC1,JU2800 -o {output} -O z {path}", shell = True)
+subprocess.Popen(f"bcftools view -S /proj/snic2022-6-224/private/Applied-Bioinformatics/1-data/strains.txt -o {output} -O z {path}", shell = True)
